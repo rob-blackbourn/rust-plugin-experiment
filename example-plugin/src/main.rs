884cc889ab598147f7646d6939c4aad9ecb0501c
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-21 10:20:07 +0000

    wip

--- example-plugin/src/main.rs
+++ example-plugin/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    eprintln!("starting plugin");
+
+    let mut buffer = String::new();
+    let mut ok = true;
+    while ok {
+        let bytes_read = io::stdin().read_line(&mut buffer)?;
+        if bytes_read == 0 {
+            ok = false;
+            continue;
+        }
+        let line = buffer.trim();
+        if bytes_read == 0 || line == "EXIT" {
+            ok = false;
+            continue;
+        }
+        eprintln!("received and echoing \"{}\"", line);
+        println!("{}", line);
+    }
+
+    eprintln!("plugin exiting");
+
+    Ok(())
 }
--- example-service/src/main.rs
+++ example-service/src/main.rs
@@ -1,15 +1,35 @@
-use std::io;
+use std::io::{self, Read, Write};
+use std::process::{Command, Stdio};
 
 fn main() -> io::Result<()> {
+    let mut plugin = Command::new("./example-plugin")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()
+        .expect("should start plugin");
+
+    let mut plugin_stdin = plugin.stdin.take().expect("should open plugin stdin");
+    let mut plugin_stdout = plugin.stdout.take().expect("should open plugin stdout");
+
+
     let mut buffer = String::new();
     let mut ok = true;
     while ok {
         println!("Type \"EXIT\" to quit");
         let bytes_read = io::stdin().read_line(&mut buffer)?;
-        if bytes_read == 0 || buffer.trim() == "EXIT" {
+        if bytes_read == 0 {
             ok = false;
+            continue;
         }
-        print!("{}", buffer);
+        let line = buffer.trim();
+        if bytes_read == 0 || line == "EXIT" {
+            ok = false;
+            continue;
+        }
+        println!("line: \"{}\"", line);
+        plugin_stdin.write(buffer.as_bytes())?;
+        plugin_stdout.read_to_string(&mut buffer)?;
+        println!("Received: {}", buffer);
     }
 
     println!("Existed normally");
