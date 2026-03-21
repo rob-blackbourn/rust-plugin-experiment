a703d1c70b7d5ad8c39a2f42ed82e2be1a4f0e33
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-21 09:35:14 +0000

    wip

--- example-service/src/main.rs
+++ example-service/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    let mut ok = true;
+    while ok {
+        println!("Type \"EXIT\" to quit");
+        let bytes_read = io::stdin().read_line(&mut buffer)?;
+        if bytes_read == 0 || buffer.trim() == "EXIT" {
+            ok = false;
+        }
+        print!("{}", buffer);
+    }
+
+    println!("Existed normally");
+
+    Ok(())
 }
