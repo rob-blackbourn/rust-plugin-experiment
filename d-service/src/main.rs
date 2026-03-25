3b84a1485202b2d4ae1fd195a3b9c1e0bd01aa01
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-25 18:54:02 +0000

    tidy

--- d-service/src/main.rs
+++ d-service/src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::io::{self, BufRead, BufReader, Write};
+use std::io::{self, BufRead, Write};
 use std::path::Path;
-use std::process::{Child, ChildStdin, ChildStdout, Command, Stdio};
 
 use d_common::{Credentials, Status};
 
@@ -10,6 +9,53 @@ use args::Args;
 
 mod plugin;
 use plugin::Plugin;
+
+fn main() -> io::Result<()> {
+    let args = Args::load()?;
+
+    add_plugin_path(&args.plugin_path);
+
+    let mut plugin = Plugin::new(args.plugin_cmdline);
+
+    let credentials = read_credentials()?;
+
+    send_credentials_to_plugin(&credentials, &mut plugin)?;
+
+    let status = receive_status_from_plugin(&mut plugin)?;
+
+    match status.ok {
+        true => println!("service: authenticated"),
+        false => println!("service: incorrect username or password"),
+    }
+
+    Ok(())
+}
+
+fn read_credentials() -> io::Result<Credentials> {
+    let username = input("username: ")?;
+    let password = input("password: ")?;
+
+    Ok(Credentials {
+        username: username.trim().to_string(),
+        password: password.trim().to_string(),
+    })
+}
+
+fn send_credentials_to_plugin(credentials: &Credentials, plugin: &mut Plugin) -> io::Result<()> {
+    let text = serde_json::to_string(&credentials)?;
+
+    plugin.stdin.write(text.as_bytes())?;
+    plugin.stdin.write(b"\n")?;
+
+    Ok(())
+}
+
+fn receive_status_from_plugin(plugin: &mut Plugin) -> io::Result<Status> {
+    let mut plugin_buffer = String::new();
+    plugin.reader.read_line(&mut plugin_buffer)?;
+    let status: Status = serde_json::from_str(&plugin_buffer)?;
+    Ok(status)
+}
 
 fn add_plugin_path(path: &str) -> () {
     let key = "PATH";
@@ -41,34 +87,4 @@ fn input(prompt: &str) -> io::Result<String> {
         return Err(io::Error::new(io::ErrorKind::Other, "Exit"));
     }
     Ok(text)
-}
-
-fn main() -> io::Result<()> {
-    let args = Args::load()?;
-
-    add_plugin_path(&args.plugin_path);
-
-    let mut plugin = Plugin::new(args.plugin_cmdline);
-
-    let username = input("username: ")?;
-    let password = input("password: ")?;
-
-    let credentials = Credentials {
-        username: username.trim().to_string(),
-        password: password.trim().to_string(),
-    };
-    let text = serde_json::to_string(&credentials)?;
-
-    plugin.stdin.write(text.as_bytes())?;
-    plugin.stdin.write(b"\n")?;
-
-    let mut plugin_buffer = String::new();
-    plugin.reader.read_line(&mut plugin_buffer)?;
-    let status: Status = serde_json::from_str(&plugin_buffer)?;
-    match status.ok {
-        true => println!("service: authenticated"),
-        false => println!("service: incorrect username or password"),
-    }
-
-    Ok(())
 }
