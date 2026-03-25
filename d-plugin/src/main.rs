366b553501edc34be776064d6b9973dfd299324c
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-25 18:32:26 +0000

    tidy

--- d-plugin/src/main.rs
+++ d-plugin/src/main.rs
@@ -8,23 +8,6 @@ use args::Args;
 mod authenticator;
 use authenticator::HtpasswdAuthenticator;
 
-fn read_credentials() -> io::Result<Option<Credentials>> {
-    let mut buffer = String::new();
-    let bytes_read = io::stdin().read_line(&mut buffer)?;
-    if bytes_read == 0 {
-        return Ok(None);
-    }
-    let credentials: Credentials = serde_json::from_str(&buffer)?;
-    Ok(Some(credentials))
-}
-
-fn write_status(status: Status) -> io::Result<()> {
-    let text = serde_json::to_string(&status)?;
-    io::stdout().write_all(text.as_bytes())?;
-    io::stdout().write_all(b"\n")?;
-    Ok(())
-}
-
 fn main() -> io::Result<()> {
     let args = Args::load()?;
     let authenticator = HtpasswdAuthenticator::new(&args.password_file)?;
@@ -39,5 +22,22 @@ fn main() -> io::Result<()> {
         write_status(Status { ok })?;
     }
 
+    Ok(())
+}
+
+fn read_credentials() -> io::Result<Option<Credentials>> {
+    let mut buffer = String::new();
+    let bytes_read = io::stdin().read_line(&mut buffer)?;
+    if bytes_read == 0 {
+        return Ok(None);
+    }
+    let credentials: Credentials = serde_json::from_str(&buffer)?;
+    Ok(Some(credentials))
+}
+
+fn write_status(status: Status) -> io::Result<()> {
+    let text = serde_json::to_string(&status)?;
+    io::stdout().write_all(text.as_bytes())?;
+    io::stdout().write_all(b"\n")?;
     Ok(())
 }
