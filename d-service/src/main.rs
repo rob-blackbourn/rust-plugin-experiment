279821105869aa5bd7c239a6f234b7e034ca1636
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-25 18:58:05 +0000

    tidy

--- d-service/src/main.rs
+++ d-service/src/main.rs
@@ -1,6 +1,4 @@
-use std::env;
 use std::io::{self, BufRead, Write};
-use std::path::Path;
 
 use d_common::{Credentials, Status};
 
@@ -9,6 +7,9 @@ use args::Args;
 
 mod plugin;
 use plugin::Plugin;
+
+mod utils;
+use utils::{add_plugin_path, input};
 
 fn main() -> io::Result<()> {
     let args = Args::load()?;
@@ -55,36 +56,4 @@ fn receive_status_from_plugin(plugin: &mut Plugin) -> io::Result<Status> {
     plugin.reader.read_line(&mut plugin_buffer)?;
     let status: Status = serde_json::from_str(&plugin_buffer)?;
     Ok(status)
-}
-
-fn add_plugin_path(path: &str) -> () {
-    let key = "PATH";
-    match env::var_os(key) {
-        Some(path_var) => {
-            let mut paths = env::split_paths(&path_var).collect::<Vec<_>>();
-            paths.push(path.into());
-            let new_path = env::join_paths(paths).expect("should join paths");
-            unsafe {
-                env::set_var(key, &new_path);
-            }
-        }
-        None => {
-            let paths = vec![Path::new(path)];
-            let path_var = env::join_paths(paths.iter()).expect("should join path");
-            unsafe {
-                env::set_var(key, &path_var);
-            }
-        }
-    }
-}
-
-fn input(prompt: &str) -> io::Result<String> {
-    print!("{}", prompt);
-    io::stdout().flush()?;
-    let mut text = String::new();
-    let bytes_read = io::stdin().read_line(&mut text)?;
-    if bytes_read == 0 {
-        return Err(io::Error::new(io::ErrorKind::Other, "Exit"));
-    }
-    Ok(text)
 }
---
+++ d-service/src/utils.rs
@@ -0,0 +1,35 @@
+use std::env;
+use std::io::{self, Write};
+use std::path::Path;
+
+pub fn add_plugin_path(path: &str) -> () {
+    let key = "PATH";
+    match env::var_os(key) {
+        Some(path_var) => {
+            let mut paths = env::split_paths(&path_var).collect::<Vec<_>>();
+            paths.push(path.into());
+            let new_path = env::join_paths(paths).expect("should join paths");
+            unsafe {
+                env::set_var(key, &new_path);
+            }
+        }
+        None => {
+            let paths = vec![Path::new(path)];
+            let path_var = env::join_paths(paths.iter()).expect("should join path");
+            unsafe {
+                env::set_var(key, &path_var);
+            }
+        }
+    }
+}
+
+pub fn input(prompt: &str) -> io::Result<String> {
+    print!("{}", prompt);
+    io::stdout().flush()?;
+    let mut text = String::new();
+    let bytes_read = io::stdin().read_line(&mut text)?;
+    if bytes_read == 0 {
+        return Err(io::Error::new(io::ErrorKind::Other, "Exit"));
+    }
+    Ok(text)
+}
