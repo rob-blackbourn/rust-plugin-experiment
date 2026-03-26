d199547d9dea274cf24f586cd74509919d598656
Rob Blackbourn <rob.blackbourn@gmail.com>
2026-03-26 07:01:51 +0000

    tidy

--- d-service/src/plugin.rs
+++ d-service/src/plugin.rs
@@ -8,25 +8,27 @@ pub struct Plugin {
 
 impl Plugin {
     pub fn new(cmdline: Vec<String>) -> io::Result<Self> {
-        let mut child = Command::new("d-plugin")
+        let child = Command::new("d-plugin")
             .args(cmdline)
             .stdin(Stdio::piped())
             .stdout(Stdio::piped())
             .spawn()
             .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
 
-        let stdin = child
-            .stdin
-            .take()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "failed to open stdin"))?;
+        let Some(stdin) = child.stdin else {
+            return Err(io::Error::new(io::ErrorKind::Other, "failed to open stdin"));
+        };
 
-        let stdout = child
-            .stdout
-            .take()
-            .ok_or_else(|| io::Error::new(io::ErrorKind::Other, "failed to open stdout"))?;
+        let Some(stdout) = child.stdout else {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "failed to open stdout",
+            ));
+        };
 
-        let reader = BufReader::new(stdout);
-
-        Ok(Self { stdin, reader })
+        Ok(Self {
+            stdin,
+            reader: BufReader::new(stdout),
+        })
     }
 }
